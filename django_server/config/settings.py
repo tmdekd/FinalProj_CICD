306ce5f8c2dd11ed5d6782d745c1c0187e840753
seunggi-coding <moonsys010103@gmail.com>
2025-07-03 09:46:12 +0900

    STATIC_ROOT 값과 STATIC_PATH 값을 동일하게 해줌

--- django_server/config/settings.py
+++ django_server/config/settings.py
@@ -212,7 +212,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (STATIC_PATH,)
 
